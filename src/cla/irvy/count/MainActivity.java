d2179644df90fa1a2bcea73b6bf938a9221d87e6
NAGAYA Shinichiro <clairvy@gmail.com>
2011-02-19 10:45:46 +0900

    とりあえず，ボタンが動くとこまで

--- src/cla/irvy/count/MainActivity.java
+++ src/cla/irvy/count/MainActivity.java
@@ -2,12 +2,28 @@ package cla.irvy.count;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
+/**
+ * @author clairvy
+ */
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        final TextView textView = (TextView)findViewById(R.id.textview01);
+        Button button = (Button)findViewById(R.id.button01);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(R.string.count);
+            }
+        });
     }
 }
--- test/src/cla/irvy/count/test/MainActivityTest.java
+++ test/src/cla/irvy/count/test/MainActivityTest.java
@@ -1,7 +1,12 @@
 package cla.irvy.count.test;
 
-import cla.irvy.count.MainActivity;
+import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.MotionEvent;
+import android.widget.Button;
+import android.widget.TextView;
+import cla.irvy.count.MainActivity;
+import cla.irvy.count.R;
 
 /**
  * @author clairvy
@@ -16,9 +21,29 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     }
 
     /**
-     * とりあえず通すテスト
+     * テストのサンプル
+     *
+     * とりあえず，今の機能をテストするよ
      */
-    public void testAAA() {
-        assertTrue(true);
+    public void testButtonPress() {
+        MainActivity activity = getActivity();
+        Instrumentation instrumentation = getInstrumentation();
+        TextView textView = (TextView)activity.findViewById(R.id.textview01);
+
+        // initial statement
+        assertEquals(activity.getString(R.string.hello), textView.getText());
+
+        final Button button = (Button)activity.findViewById(R.id.button01);
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                button.dispatchTouchEvent(MotionEvent.obtain(5, 0, MotionEvent.ACTION_DOWN, 0, 0, 0));
+                button.dispatchTouchEvent(MotionEvent.obtain(5, 0, MotionEvent.ACTION_UP, 0, 0, 0));
+            }
+        });
+        instrumentation.waitForIdleSync();
+
+        // result statement
+        assertEquals(activity.getString(R.string.count), textView.getText());
     }
 }
